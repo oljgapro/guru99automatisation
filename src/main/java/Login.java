3e95bd2107e0450f4fd47c2b5f648f2b2df49a53
oljgapro <oljga.prokopenko@gmail.com>
2021-10-19 10:44:49 +0300

    test

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -1,37 +1,59 @@
+
 import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.NoAlertPresentException;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class Login extends Common
 {
-    private static String actualTitle;
-    private static String actualBoxtitle;
-    public static void main(String[] args) {
+
+
+    public void Main() throws Exception {
+        String[][] testData = Util.getDataFromExcel(Util.FILE_PATH,
+                Util.SHEET_NAME, Util.TABLE_NAME);
+        String username, password;
+        String actualTitle;
+        String actualBoxtitle;
+
+        @BeforeTest
         Common common = new Common();
-        common.setUP();
+                common.setUP();
         common.startBrowser();
-        common.login(Util.INVALID_USER_NAME, Util.INVALID_PASS);
+
+        for (int i = 0; i < testData.length; i++) {
+            username = testData[i][0]; // get username
+            password = testData[i][1]; // get password
+
+
+
+@Test
+        common.login(username, password);
         try{
 
             Alert alt = driver.switchTo().alert();
             actualBoxtitle = alt.getText(); // get content of the Alter Message
             alt.accept();
             if (actualBoxtitle.contains(Util.EXPECT_ERROR)) { // Compare Error Text with Expected Error Value
-                System.out.println("Test case : Passed");
+                System.out.println("Test case with error message SS[" + i + "]: Passed");
             } else {
-                System.out.println("Test case: Failed");
+                System.out.println("Test case SS[" + i + "]: Failed");
             }
         }
         catch (NoAlertPresentException Ex){
             actualTitle = driver.getTitle();
             // On Successful login compare Actual Page Title with Expected Title
             if (actualTitle.contains(Util.EXPECT_TITLE)) {
-                System.out.println("Test case SS: Passed");
+                System.out.println("Test case without error SS[" + i + "]: Passed");
             } else {
-                System.out.println("Test case SS: Failed");
+                System.out.println("Test case SS[" + i + "]: Failed");
             }
 
+
         }
+@AfterTest
+        common.closeBrowser();
     }
 
    /*     String actualTitle = driver.getTitle();
@@ -48,4 +70,5 @@ public class Login extends Common
 
 
     }
+}
 
--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,11 +1,51 @@
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import java.io.File;
+
+
 public class Util {
     public static final String CHROM_PATH = "C:\\Users\\Olga\\IdeaProjects\\javaguru\\src\\test\\resources\\";
     public static final String BASE_URL = "http://www.demo.guru99.com/";
-    public static final int WAIT_TIME=30;
-    public static final String VALID_USER_NAME="mngr358985";
-public static final String INVALID_USER_NAME="1234";
+    public static final int WAIT_TIME = 30;
+    public static final String VALID_USER_NAME = "mngr358985";
+    public static final String INVALID_USER_NAME = "1234";
     public static final String VALID_PASS = "pAbasEg";
     public static final String INVALID_PASS = "1234";
     public static final String EXPECT_TITLE = "Guru99 Bank Manager HomePage";
     public static final String EXPECT_ERROR = "User or Password is not valid";
+    public static final String FILE_PATH = "testData.xls"; // File Path
+    public static final String SHEET_NAME = "Data"; // Sheet name
+    public static final String TABLE_NAME = "testData"; // Name of data table
+
+    public static String[][] getDataFromExcel(String xlFilePath, String sheetName, String tableName) throws Exception {
+
+        String[][] tabArray = null;
+        Workbook workbook = Workbook.getWorkbook(new File(xlFilePath));
+        Sheet sheet = workbook.getSheet(sheetName);
+
+        int startRow, startCol, endRow, endCol, ci, cj;
+        Cell tableStart = sheet.findCell(tableName);
+        startRow = tableStart.getRow();
+        startCol = tableStart.getColumn();
+
+        Cell tableEnd = sheet.findCell(tableName, startCol + 1, startRow + 1,
+                100, 64000, false);
+        endRow = tableEnd.getRow();
+        endCol = tableEnd.getColumn();
+
+        tabArray = new String[endRow - startRow - 1][endCol - startCol - 1];
+        ci = 0;
+
+        // Store all data in an array
+        for (int i = startRow + 1; i < endRow; i++, ci++) {
+            cj = 0;
+            for (int j = startCol + 1; j < endCol; j++, cj++) {
+                //Get content of each cell and store to each array element.
+                tabArray[ci][cj] = sheet.getCell(j, i).getContents();
+            }
+        }
+        return (tabArray);
+    }
+
 }
